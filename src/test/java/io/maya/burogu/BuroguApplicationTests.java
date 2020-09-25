a424e649e88d5f215019f9cfa676112b5c1ada46
Lalo Espino <edespinom@gmail.com>
2020-09-24 23:23:49 -0500

    Initial commit

---
+++ src/test/java/io/maya/burogu/BuroguApplicationTests.java
@@ -0,0 +1,13 @@
+package io.maya.burogu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuroguApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
